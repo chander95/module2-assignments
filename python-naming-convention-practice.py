af98ac91d86eab3ce00c4ec577671ccb75651bb6
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-06 14:20:58 -0400

    this is the corrected naming convention for task 1 in question 2. Question 2 is completed.

--- python-naming-convention-practice.py
+++ python-naming-convention-practice.py
@@ -1,6 +1,6 @@
 # Task 1: Code Correction You have been given a piece of code with several variable and constant names that don't follow the Python naming convention. Your task is to correct them:
 
-Pi_value = 3.14
+PI = 3.14
 userAge = 25
 user_location = "New York"
-MAXLIMIT = 1000
+max_limit = 1000
