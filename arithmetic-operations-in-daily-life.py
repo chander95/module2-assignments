a9b1b7e0d86dec27a31765b667aeb3d94502963f
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-06 14:23:23 -0400

    task 1 completed.

---
+++ arithmetic-operations-in-daily-life.py
@@ -0,0 +1,8 @@
+#Task 1: Grocery Store Math Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. For example, what would be the cost of bread, peanut butter, and jelly be? Prices don't need to be realistic!
+
+bread = 3.99
+peanut_butter = 6.99
+jelly = 5.99
+
+total = bread + peanut_butter + jelly
+print(total)
