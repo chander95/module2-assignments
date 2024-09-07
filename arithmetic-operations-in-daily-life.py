6d073ca8c76a092cb2465c1ea21e1aaea210b561
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-07 11:49:19 -0400

    minor change to easily differentiate task 1 from task 2

--- arithmetic-operations-in-daily-life.py
+++ arithmetic-operations-in-daily-life.py
@@ -1,3 +1,5 @@
+print("-------------------------------------------------------")
+
 #Task 1: Grocery Store Math Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. For example, what would be the cost of bread, peanut butter, and jelly be? Prices don't need to be realistic!
 
 bread = 3.99
@@ -5,7 +7,9 @@ peanut_butter = 6.99
 jelly = 5.99
 
 total = bread + peanut_butter + jelly
-print(total)
+print("Task 1: " + str(total))
+
+print("-------------------------------------------------------")
 
 #Task 2: Bank Interest If you have a savings account with a particular initial amount and a fixed yearly interest rate, calculate the total amount in your account after a year. So if you had $10,000 at a 7% interest write code that would tell us the amount at the end of the year. For the example the expected outcome would be $10,700.
 
@@ -14,4 +18,7 @@ interest_rate = amount * 0.07 * 1
 time = 12
 
 roi = amount + interest_rate
-print(roi)
+print("Task 2: " + str(roi))
+
+
+print("-------------------------------------------------------")
