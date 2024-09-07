e48169f2b945387ada2a60fcf0f59a6d7471f444
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-07 11:47:02 -0400

    Task 2 in question 3 completed.

--- arithmetic-operations-in-daily-life.py
+++ arithmetic-operations-in-daily-life.py
@@ -6,3 +6,12 @@ jelly = 5.99
 
 total = bread + peanut_butter + jelly
 print(total)
+
+#Task 2: Bank Interest If you have a savings account with a particular initial amount and a fixed yearly interest rate, calculate the total amount in your account after a year. So if you had $10,000 at a 7% interest write code that would tell us the amount at the end of the year. For the example the expected outcome would be $10,700.
+
+amount = 10000
+interest_rate = amount * 0.07 * 1
+time = 12
+
+roi = amount + interest_rate
+print(roi)
